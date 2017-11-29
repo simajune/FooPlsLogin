6231aaf7c37a86bfb9b7a36c1c074603b09f45e5
ssy828 <828yss@gmail.com>
2017-11-29 16:36:33 +0900

    update LoginSignup part

---
+++ LogInSignUpProc/Signup/VC/SignUpViewController.swift
@@ -0,0 +1,15 @@
+
+// SignUpVC
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
