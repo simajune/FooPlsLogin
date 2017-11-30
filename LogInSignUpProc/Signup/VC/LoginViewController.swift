651ecca6002e6bf26e1a131c9ebbc99161838904
simajune <tejayjung@gmail.com>
2017-11-30 17:46:34 +0900

    kakao error

--- LogInSignUpProc/Signup/VC/LoginViewController.swift
+++ LogInSignUpProc/Signup/VC/LoginViewController.swift
@@ -11,6 +11,13 @@ class LoginViewController: UIViewController {
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+//        do {
+//            try Auth.auth().signOut()
+//            KOSession.shared().close()
+//        }catch let error{
+//            print(error)
+//        }
+        
         loginBtn.layer.cornerRadius  = loginBtn.frame.size.height / 2
         facebookBtn.layer.cornerRadius  = facebookBtn.frame.size.height / 2
         kakaoBtn.layer.cornerRadius  = kakaoBtn.frame.size.height / 2
@@ -20,15 +27,28 @@ class LoginViewController: UIViewController {
         KOSession.shared().close()
         KOSession.shared().open { (error) in
             if error != nil {
-                print(error!.localizedDescription)
+                print(error?.localizedDescription)
             }
+            
             if KOSession.shared().isOpen() {
-                print("AccessToken: ", KOSession.shared().accessToken)
-                self.requestCustomToken(accessToken: KOSession.shared().accessToken)
+                self.requestFirebaseJwt(accessToken: KOSession.shared().accessToken)
             } else {
                 print("login failed: \(error!)")
             }
         }
+        
+//        KOSession.shared().close()
+//        KOSession.shared().open { (error) in
+//            if error != nil {
+//                print(error!.localizedDescription)
+//            }
+//            if KOSession.shared().isOpen() {
+//                print("AccessToken: ", KOSession.shared().accessToken)
+//                self.requestCustomToken(accessToken: KOSession.shared().accessToken)
+//            } else {
+//                print("login failed: \(error!)")
+//            }
+//        }
         
     }
     
@@ -40,49 +60,104 @@ class LoginViewController: UIViewController {
         
     }
     
-    //MARK: - request firebase custom token
-    private func requestCustomToken(accessToken: String) {
+    
+    /**
+     Request firebase token from the validation server.
+     */
+    func requestFirebaseJwt(accessToken: String) {
         let url = URL(string: String(format: "%@/verifyToken", Bundle.main.object(forInfoDictionaryKey: "VALIDATION_SERVER_URL") as! String))!
+        print(url)
         var urlRequest = URLRequest(url: url)
         urlRequest.httpMethod = "POST"
         urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
         urlRequest.setValue("application/json", forHTTPHeaderField: "Accept")
         
-        let token = KOSession.shared().accessToken
-        let parameter = ["token": token]
+        
+        let token = KOSession.shared().accessToken!
+        let parameters: [String: String] = ["token": token]
         
         do {
-            let jsonParams = try JSONSerialization.data(withJSONObject: parameter, options: [])
+            let jsonParams = try JSONSerialization.data(withJSONObject: parameters, options: [])
             urlRequest.httpBody = jsonParams
-        }catch {
+        } catch {
             print("Error in adding token as a parameter: \(error)")
         }
         
         URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
             guard let data = data, error == nil else {
-                print("Error in request token verify: \(error!)")
+                print("Error in request token verifying: \(error!)")
                 return
             }
             do {
                 let jsonResponse = try JSONSerialization.jsonObject(with: data, options: []) as! [String: String]
                 let firebaseToken = jsonResponse["firebase_token"]!
                 self.signInToFirebaseWithToken(firebaseToken: firebaseToken)
-            }catch let error {
+            } catch let error {
                 print("Error in parsing token: \(error)")
             }
-        }.resume()
+            
+            }.resume()
     }
     
+    /**
+     Sign in to Firebse with the custom token generated from the validation server.
+     
+     Performs segue if signed in successfully.
+     */
     func signInToFirebaseWithToken(firebaseToken: String) {
         print(firebaseToken)
         Auth.auth().signIn(withCustomToken: firebaseToken) { (user, error) in
             if let authError = error {
                 print("authError",authError)
             } else {
-                self.performSegue(withIdentifier: "mainSegue", sender: self)
+                self.performSegue(withIdentifier: "loginSegue", sender: self)
             }
         }
     }
+    
+//    //MARK: - request firebase custom token
+//    private func requestCustomToken(accessToken: String) {
+//        let url = URL(string: String(format: "%@/verifyToken", Bundle.main.object(forInfoDictionaryKey: "VALIDATION_SERVER_URL") as! String))!
+//        var urlRequest = URLRequest(url: url)
+//        urlRequest.httpMethod = "POST"
+//        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+//        urlRequest.setValue("application/json", forHTTPHeaderField: "Accept")
+//
+//        let token = KOSession.shared().accessToken!
+//        let parameter = ["token": token]
+//
+//        do {
+//            let jsonParams = try JSONSerialization.data(withJSONObject: parameter, options: [])
+//            urlRequest.httpBody = jsonParams
+//        }catch {
+//            print("Error in adding token as a parameter: \(error)")
+//        }
+//
+//        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+//            guard let data = data, error == nil else {
+//                print("Error in request token verify: \(error!)")
+//                return
+//            }
+//            do {
+//                let jsonResponse = try JSONSerialization.jsonObject(with: data, options: []) as! [String: String]
+//                let firebaseToken = jsonResponse["firebase_token"]!
+//                self.signInToFirebaseWithToken(firebaseToken: firebaseToken)
+//            }catch let error {
+//                print("Error in parsing token: \(error)")
+//            }
+//        }.resume()
+//    }
+//
+//    func signInToFirebaseWithToken(firebaseToken: String) {
+//        print(firebaseToken)
+//        Auth.auth().signIn(withCustomToken: firebaseToken) { (user, error) in
+//            if let authError = error {
+//                print("authError",authError)
+//            } else {
+//                self.performSegue(withIdentifier: "mainSegue", sender: self)
+//            }
+//        }
+//    }
 }
 
 // MARK: FBSDKLoginButtonDelegate
