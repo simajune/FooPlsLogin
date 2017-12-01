b8dd2bfa08bdb40e3a3488c3acadf56d3b5abfcc
simajune <tejayjung@gmail.com>
2017-12-01 10:26:58 +0900

    Add Remark

--- LogInSignUpProc/Splash/SplashViewController.swift
+++ LogInSignUpProc/Splash/SplashViewController.swift
@@ -5,11 +5,13 @@
 import UIKit
 import SwiftKeychainWrapper
 import Firebase
+import FBSDKLoginKit
 
 class SplashViewController: UIViewController {
     
     // MARK: property
     let loginSegue = "loginSegue"
+    let mainSegue = "mainSegue"
     
     @IBOutlet weak var fooplsLoadingView: FOOPLSView!
 
@@ -17,12 +19,15 @@ class SplashViewController: UIViewController {
     override func viewDidLoad() {        
         super.viewDidLoad()
         try! Auth.auth().signOut()
+        FBSDKLoginManager().logOut()
+        
+        //MARK: - 애니메이션 동작, 동작 후 후행 클로저를 통해 현재 로그인이 되어 있는지 확인
         fooplsLoadingView.addLoadingAnimation { [weak self] (action) in
             guard let `self` = self else { return }
             if let _ = Auth.auth().currentUser {
-                self.performSegue(withIdentifier: "mainSegue", sender: self)
+                self.performSegue(withIdentifier: self.mainSegue, sender: self)
             }else {
-                self.performSegue(withIdentifier: "loginSegue", sender: self)
+                self.performSegue(withIdentifier: self.loginSegue, sender: self)
             }
         }
     }
