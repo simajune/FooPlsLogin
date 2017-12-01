b7b0e580955570c84b1d94dcd5009dbcb1a78893
simajune <tejayjung@gmail.com>
2017-12-01 18:31:51 +0900

    commit

---
+++ Pods/Target Support Files/Pods-FooPls/Pods-FooPls-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FooPls : NSObject
+@end
+@implementation PodsDummy_Pods_FooPls
+@end
--- Pods/Target Support Files/Pods-FooPls/Pods-FooPls-umbrella.h
+++ Pods/Target Support Files/Pods-FooPls/Pods-FooPls-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_LogInSignUpProcVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_LogInSignUpProcVersionString[];
+FOUNDATION_EXPORT double Pods_FooPlsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FooPlsVersionString[];
 
